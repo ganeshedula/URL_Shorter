930ce4c7ddc8179ee1776d6a54d308fc2b11f864
ganesh <edulagaensh952@gmail.com>
2026-07-10 23:48:46 +0530

    Project created and add Database

---
+++ src/main/java/com/url/shortener/models/UrlMapping.java
@@ -0,0 +1,30 @@
+package com.url.shortener.models;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+
+public class UrlMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String originalUrl;
+    private String shortUrl;
+    private int clickCount = 0;
+    private LocalDateTime createdAt;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "urlMapping")
+    private List<ClickEvent> clickEvents;
+
+}
